0fc27620162e5627b398ea72493d54de68e3ae9b
SaiDeeksha29 <saideeksha29@gmail.com>
2020-11-26 22:29:58 +0530

    Added:Remove method for employee payroll

--- js/new_home.js
+++ js/new_home.js
@@ -64,12 +64,19 @@ const remove = (node) => {
     if (!empPayrollData) return;
     const index = empPayrollList.map(empData => empData.id).indexOf(empPayrollData.id);
     empPayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
-    if (empPayrollList.length == 0) {
-        location.reload();
+    if (site_properties.use_local_storage.match("true")) {
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+        createInnerHTML();
+    } else {
+        const deleteURL = site_properties.server_url + empPayrollData.id.toString();
+        makeServiceCall("DELETE", deleteURL, false)
+            .then(responseText => {
+                createInnerHTML();
+            })
+            .catch(error => {
+                console.log("DELETE error status: " + JSON.stringify(error));
+            });
     }
-    createInnerHtml();
 }
 
 const update = (node) => {
